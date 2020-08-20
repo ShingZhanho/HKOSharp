dceb2bae9b9ead6ecb5651ab55b362840ad536c5
Zhanho Shing <zhanhoshing@icloud.com>
2020-08-20 11:36:05 +0800

    Added test tool and new dependency

---
+++ DebugTool/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using HKOSharp;
+
+namespace DebugTool {
+    internal class Program {
+        public static void Main(string[] args) {
+            LibHKOSharp.Weather.GetLocalWeatherForecast(Language.English);
+            Console.Read();
+        }
+    }
+}
