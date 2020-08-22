de3a5967de760f5db18455df53750210b3c1fa1c
Zhanho Shing <zhanhoshing@icloud.com>
2020-08-22 11:43:29 +0800

    Added GetNineDaysWeather()

--- LibHKOSharp/WeatherClass/Weather.cs
+++ LibHKOSharp/WeatherClass/Weather.cs
@@ -6,6 +6,8 @@ using System.Management.Instrumentation;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HKOSharp {
     public partial class LibHKOSharp {
@@ -60,6 +62,27 @@ namespace HKOSharp {
                 return new LocalWeatherForecast(response);
             }
 
+            /// <summary>
+            /// Gets Nine-Day Weather Forecast in given language. This method will block the calling thread.
+            /// </summary>
+            /// <param name="language">Language of forecast to get</param>
+            /// <returns>NineDaysWeather object if succeeded, null instead.</returns>
+            public static NineDaysWeather GetNineDaysWeather(Language language) {
+                var requestUrl = ApiUrl;
+                requestUrl += "dataType=fnd";
+                
+                if (language == Language.English)
+                    requestUrl += "&lang=en";
+                else if (language == Language.TraditionChinese)
+                    requestUrl += "&lang=tc";
+                else if (language == Language.SimplifiedChinese) requestUrl += "&lang=sc";
+
+                // Request and get response asynchronously
+                var response =  HttpRequest(requestUrl);
+                
+                return !JsonIsValid(response) ? null : new NineDaysWeather(response);
+            }
+
             #endregion
 
             #region Methods for internal use
@@ -101,6 +124,17 @@ namespace HKOSharp {
                 }
 
                 return response;
+            }
+
+            private static bool JsonIsValid(string json) {
+                try {
+                    var jo = JObject.Parse(json);
+                }
+                catch (Exception e) {
+                    Console.WriteLine(e);
+                    return false;
+                }
+                return true;
             }
 
             #endregion
