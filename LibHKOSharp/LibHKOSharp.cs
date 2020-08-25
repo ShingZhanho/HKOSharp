6ce4ea573bb64c989fafe5f202237c8ce6fe5316
Zhanho Shing <zhanhoshing@icloud.com>
2020-08-25 22:46:34 +0800

    Several Changes
    - Deleted old project LibHKOSharp with files backed up in Temp_OldCode_dotNetFramework dir
    - Removed project DebugTool fron solution

--- LibHKOSharp/LibHKOSharp.cs
+++ LibHKOSharp/LibHKOSharp.cs
@@ -1,27 +1,5 @@
-﻿// LibHKOSharp.cs is the file for main function of LibHKOSharp
+﻿using System;
 
-using System;
-using System.Runtime.CompilerServices;
-
-namespace HKOSharp {
-    public static partial class LibHKOSharp {
-        #region Methods
-
-        internal static string GetLanguageParameter(Language language) {
-            return language switch {
-                Language.English => "&lang=en",
-                Language.TraditionChinese => "&lang=tc",
-                Language.SimplifiedChinese => "&lang=sc",
-                _ => "&lang=en"
-            };
-        }
-
-        #endregion
-    }
-
-    public enum Language {
-        English,
-        TraditionChinese,
-        SimplifiedChinese
-    }
+namespace LibHKOSharp {
+    public class Class1 { }
 }
---
+++ Temp_OldCode_dotNetFramework/LibHKOSharp/LibHKOSharp.cs
@@ -0,0 +1,27 @@
+﻿// LibHKOSharp.cs is the file for main function of LibHKOSharp
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace HKOSharp {
+    public static partial class LibHKOSharp {
+        #region Methods
+
+        internal static string GetLanguageParameter(Language language) {
+            return language switch {
+                Language.English => "&lang=en",
+                Language.TraditionChinese => "&lang=tc",
+                Language.SimplifiedChinese => "&lang=sc",
+                _ => "&lang=en"
+            };
+        }
+
+        #endregion
+    }
+
+    public enum Language {
+        English,
+        TraditionChinese,
+        SimplifiedChinese
+    }
+}
