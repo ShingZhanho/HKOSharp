32d5ae3621dbc51bed4d9cc4c4e726f551c23ec3
Zhanho Shing <zhanhoshing@icloud.com>
2020-08-28 21:27:31 +0800

    Added weather icon class

---
+++ HKOSharp/ClassesForResults/WeatherIcon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using LibHKOSharp.Properties;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HKOSharp {
+    /// <summary>
+    /// Represents a weather icon
+    /// </summary>
+    public class WeatherIcon {
+        internal WeatherIcon(int iconCode, Language language) {
+            IconCode = iconCode;
+
+            var jo = (JObject) JsonConvert.DeserializeObject(Resources.JSON_iconDesc);
+            if (jo is null) return;
+
+            IconDesc = jo["iconDescriptions"][$"{iconCode}"][$"{(int) language}"].ToString();
+
+            Icon = (Bitmap) Resources.ResourceManager.GetObject($"icon{IconCode}");
+        }
+        
+        // Fields
+        public Bitmap Icon { get; }
+        public int IconCode { get; }
+        public string IconDesc { get; }
+    }
+}
