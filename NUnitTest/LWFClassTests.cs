30bdafd2a8e2dc1e05a9270f84987a156eef1625
Zhanho Shing <zhanhoshing@icloud.com>
2020-09-01 12:44:20 +0800

    Created tests for LWF class in NUnit

---
+++ NUnitTest/LWFClassTests.cs
@@ -0,0 +1,80 @@
+﻿using HKOSharp;
+using HKOSharp.LibHKOSharp;
+using NUnit.Framework;
+
+namespace NUnitTest {
+    public class LWFClassTests {
+        
+        [Test]
+        public void Constructor_NullJSON_Fail() {
+            // Arrange
+            string nullJson = null;
+            
+            // Acts
+            var lwf = new LocalWeatherForecast(nullJson, Language.English);
+            
+            // Asserts
+            Assert.IsFalse(lwf.IsSucceeded);
+        }
+
+        [Test]
+        public void Constructor_InvalidJSON_Fail() {
+            // Arrange
+            const string invalidJson = "This json is invalid.";
+            
+            // Acts
+            var lwf = new LocalWeatherForecast(invalidJson, Language.English);
+            
+            // Asserts
+            Assert.IsFalse(lwf.IsSucceeded);
+        }
+        
+        [Test]
+        public void Constructor_FailToAssignProperties_Fail() {
+            // Arrange
+            const string wrongJsonFormat = "{\"sample\":\"not correct format\"}";
+            
+            // Acts
+            var lwf = new LocalWeatherForecast(wrongJsonFormat, Language.English);
+            
+            // Asserts
+            Assert.IsFalse(lwf.IsSucceeded);
+        }
+        
+        [Test]
+        public void ClassProperties__NotNull() {
+            // Arrange
+            var lwf = Weather.GetLocalWeatherForecast();
+
+            // Acts
+            var properties = new[] {
+                lwf.GeneralSituation,
+                lwf.TCInfo,
+                lwf.FireDangerWarning,
+                lwf.ForecastPeriod,
+                lwf.ForecastDesc,
+                lwf.Outlook,
+                lwf.UpdateTime.ToString(),
+                ((int)lwf.Language).ToString()
+            };
+            
+            // Asserts
+            foreach (var property in properties) {
+                Assert.IsNotNull(property);
+            }
+        }
+        
+        [Test]
+        public void ClassToString_AllLanguages_NotNull(
+            [Values(Language.English, Language.TraditionalChinese, Language.SimplifiedChinese)] Language language) {
+            // Arrange
+            var lwf = Weather.GetLocalWeatherForecast(language);
+            
+            // Acts
+            var lwfString = lwf.ToString();
+            
+            // Asserts
+            Assert.IsNotNull(lwfString);
+        }
+    }
+}
